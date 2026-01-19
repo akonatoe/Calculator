9b031a50f5a83ada5ad3513c75a69608e660434a
akonatoe <160513538+akonatoe@users.noreply.github.com>
2026-01-20 00:32:30 +0800

    added one small output update
    soon to add functions and more buttons working plus display update and ADD MORE NEEDED BUTTONS

--- js/calcu.js
+++ js/calcu.js
@@ -1,9 +1,14 @@
-const plus = Document.getElementById("plusBtn");
-const minus = Document.getElementById("minusBtn");
-const divide = Document.getElementById("slashBtn");
-const multiply = Document.getElementById("astBtn");
+const plus = document.getElementById("plusBtn");
+const minus = document.getElementById("minusBtn");
+const divide = document.getElementById("slashBtn");
+const multiply = document.getElementById("astBtn");
+const display = document.getElementById("display");
 
 
-const one = Document.getElementById("oneBtn");
-const two = Document.getElementById("twoBtn");
+const one = document.getElementById("oneBtn");
+const two = document.getElementById("twoBtn");
 
+one.addEventListener("click", function(){
+    display.innerText += 1;
+
+});
