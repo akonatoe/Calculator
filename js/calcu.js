62a446ab4fb4329b94950d2de67227d8542b2734
akonatoe <160513538+akonatoe@users.noreply.github.com>
2026-01-19 20:24:02 +0800

    fixed calculator layout, initial commit calcu.js

---
+++ js/calcu.js
@@ -0,0 +1,9 @@
+const plus = Document.getElementById("plusBtn");
+const minus = Document.getElementById("minusBtn");
+const divide = Document.getElementById("slashBtn");
+const multiply = Document.getElementById("astBtn");
+
+
+const one = Document.getElementById("oneBtn");
+const two = Document.getElementById("twoBtn");
+
