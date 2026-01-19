a9534c733e2cfb705277f23a5947ac068850b473
akonatoe <160513538+akonatoe@users.noreply.github.com>
2026-01-20 01:59:55 +0800

    updated calcu js
    changed how it shows up on display and added notes for future use

--- js/calcu.js
+++ js/calcu.js
@@ -9,6 +9,12 @@ const one = document.getElementById("oneBtn");
 const two = document.getElementById("twoBtn");
 
 one.addEventListener("click", function(){
-    display.innerText += 1;
+    if(display.innerText == 0){
+        display.innerText = "";
+    }
 
+    display.innerText += 1;
 });
+
+//to convert the display to number -- Number(display.innerText)
+// everytime an operator is clicked, the numerical version of the display.innertext will be received by a variable so that in the final moment when equals is clicked, it will be used to do the operations
