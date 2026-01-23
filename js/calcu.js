5ec824aac1ec50c4ff4bfad931c058f77cc457fd
akonatoe <160513538+akonatoe@users.noreply.github.com>
2026-01-23 17:10:12 +0800

    changed js structiure. more efficient way with selectqueryall and foreach loop

--- js/calcu.js
+++ js/calcu.js
@@ -4,17 +4,46 @@ const divide = document.getElementById("slashBtn");
 const multiply = document.getElementById("astBtn");
 const display = document.getElementById("display");
 
+const calcuBtn = document.querySelectorAll(".calcu-btn");
+const text = document.querySelector("#testing")
 
-const one = document.getElementById("oneBtn");
-const two = document.getElementById("twoBtn");
+calcuBtn.forEach(btn => {
+    btn.addEventListener("click", function() {
+    const btnValue = btn.innerText;
+
+
+    const operator_count = 0;
+
+    if(btnValue == '+' || btnValue == '-' || btnValue == '*' || btnValue == '/' || btnValue == "."){
+
+    }
+    
+
+    if (display.innerText == 0){
+        
+        display.innerText = "";
+        console.log(btnValue);
+    }
+
+
+    display.innerText += btnValue;
+        
+    })
+});
+
+
+
 
 one.addEventListener("click", function(){
-    if(display.innerText == 0){
-        display.innerText = "";
-    }
+    
 
     display.innerText += 1;
 });
 
+plus.addEventListener("click", function(){
+    const value = [];
+
+});
+
 //to convert the display to number -- Number(display.innerText)
-// everytime an operator is clicked, the numerical version of the display.innertext will be received by a variable so that in the final moment when equals is clicked, it will be used to do the operations
+// everytime an operator is clicked, the numerical version of the display.innertext will be received by a variable so that in the final moment when equals is clicked, it will be used to do the operation
