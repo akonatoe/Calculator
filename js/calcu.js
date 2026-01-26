3f7a48c4f5b368c8ab5c2d601d0c4395f2055600
akonatoe <160513538+akonatoe@users.noreply.github.com>
2026-01-27 00:28:48 +0800

    POLISHED THE DISPLAY LOGIC IN JS(still 15 buttons no equal)

--- js/calcu.js
+++ js/calcu.js
@@ -1,49 +1,57 @@
-const plus = document.getElementById("plusBtn");
-const minus = document.getElementById("minusBtn");
-const divide = document.getElementById("slashBtn");
-const multiply = document.getElementById("astBtn");
 const display = document.getElementById("display");
 
 const calcuBtn = document.querySelectorAll(".calcu-btn");
-const text = document.querySelector("#testing")
+
+
+let operatorCount = 0;
+let operatorList = [];
 
 calcuBtn.forEach(btn => {
+
+
     btn.addEventListener("click", function() {
-    const btnValue = btn.innerText;
-
-
-    const operator_count = 0;
-
-    if(btnValue == '+' || btnValue == '-' || btnValue == '*' || btnValue == '/' || btnValue == "."){
-
-    }
-    
-
-    if (display.innerText == 0){
+        const btnValue = btn.innerText;
         
-        display.innerText = "";
-        console.log(btnValue);
-    }
+        const symbols = ['+', '-', '*', '/', '.']; //used as condition only
+
+        const operators = ['+', '-', '*', '/'] //used for checking operators only
+        
+
+        if (display.innerText == '0'){
+            if (symbols.includes(btnValue)){
+                display.innerText += btnValue;
+                
+                operatorCount++;
+                operatorList.push(btnValue);    
+                // need malaman ilang operator and pangilang index sa string yung mga symbols
+            }
+            else{
+                display.innerText = btnValue;
+                return;
+            }
+        } //FOR 0-START validation [MAIN IF 1]
 
 
-    display.innerText += btnValue;
+        //if last index sa dislplay is SYMBOL / OPERATOR, then change it to the new operator
+        if (symbols.includes(display.innerText.slice(-1))){
+            if(symbols.includes(btnValue)){
+                display.innerText = display.innerText.slice(0, -1);
+                display.innerText += btnValue;
+            }
+            else if(!symbols.includes(btnValue)){
+                display.innerText += btnValue; 
+            }            
+        }//MAIN IF 2
+
+        //if display is NOT 0 or if the last char is NOT AN OPERATOR, meaning a number
+        else if (!symbols.includes(display.innerText.slice(-1)) && display.innerText.length > 1){
+            display.innerText += btnValue;
+        }//ELSE IF OF MAIN 2
+
+        if (display.innerText.length == 1){
+            display.innerText += btnValue;
+        }
+
         
     })
 });
-
-
-
-
-one.addEventListener("click", function(){
-    
-
-    display.innerText += 1;
-});
-
-plus.addEventListener("click", function(){
-    const value = [];
-
-});
-
-//to convert the display to number -- Number(display.innerText)
-// everytime an operator is clicked, the numerical version of the display.innertext will be received by a variable so that in the final moment when equals is clicked, it will be used to do the operation
